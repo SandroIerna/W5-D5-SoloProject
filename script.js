3ea6dd260c7c12ecb2dc3879c66306a032c645a7
SandroIerna <iernasandro@gmail.com>
2022-10-28 16:42:46 +0200

    adds not randomized structure

---
+++ script.js
@@ -0,0 +1,80 @@
+let addToList = function () {
+  let input = document.getElementById("name");
+  if (input.value !== "") {
+    let list = document.getElementById("name-list");
+    let nameInList = document.createElement("div");
+    nameInList.classList.add("listed-name");
+    nameInList.innerText = input.value;
+    list.appendChild(nameInList);
+    input.value = "";
+  }
+};
+
+let numberOfTeams = document.getElementById("number-of-teams");
+let teamNumber = 0;
+
+let generateTeams = function () {
+  let displayTeams = document.getElementById("teams-display");
+  displayTeams.innerHTML = "";
+  for (let i = 0; i < teamNumber; i++) {
+    let team = document.createElement("div");
+    let teamNumber = document.createElement("div");
+    let teamCard = document.createElement("div");
+    teamNumber.classList.add("team-number");
+    teamCard.classList.add("team-card");
+    let h2 = document.createElement("h2");
+    h2.innerText = "Team " + (i + 1);
+    teamNumber.appendChild(h2);
+    team.appendChild(teamNumber);
+    team.appendChild(teamCard);
+    displayTeams.appendChild(team);
+  }
+};
+
+let add1Team = function () {
+  teamNumber++;
+  numberOfTeams.innerText = teamNumber;
+  generateTeams();
+};
+
+let remove1Team = function () {
+  if (teamNumber > 0) {
+    teamNumber--;
+    numberOfTeams.innerText = teamNumber;
+  }
+  generateTeams();
+};
+
+let assignMember = function () {
+  let arrayOfNames = document.getElementsByClassName("listed-name");
+  if (teamNumber !== 0) {
+    let j = 0;
+    for (let i = 0; i < arrayOfNames.length; i++) {
+      let name = arrayOfNames[i].innerText;
+      let teamCard = document.getElementsByClassName("team-card");
+      if (j < teamNumber) {
+        let div = document.createElement("div");
+        div.innerText = name;
+        teamCard[j].appendChild(div);
+        j++;
+      } else {
+        j = 0;
+        let div = document.createElement("div");
+        div.innerText = name;
+        teamCard[j].appendChild(div);
+        j++;
+      }
+    }
+    let list = document.getElementById("name-list");
+    list.innerHTML = "";
+  }
+};
+
+let reset = function () {
+  let displayTeams = document.getElementById("teams-display");
+  let list = document.getElementById("name-list");
+  teamNumber = 0;
+  numberOfTeams.innerText = 0;
+  displayTeams.innerHTML = "";
+  list.innerHTML = "";
+};
